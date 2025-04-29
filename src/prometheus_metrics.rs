a5024b59f45bd3b6bb905d11420893763b1bbb91
Gregory Cavelier <gregory.cavelier@cnieg.fr>
2025-04-29 14:20:13 +0200

    add *metric_name_start() tests

--- src/prometheus_metrics.rs
+++ src/prometheus_metrics.rs
@@ -95,3 +95,99 @@ pub fn build(gitlab_token: Token) -> Result<String, Box<dyn Error + Send + Sync>
     res.push_str(&metric_str);
     Ok(res)
 }
+
+//-------------------------------------------
+//
+// Unit tests
+//
+// ------------------------------------------
+
+#[cfg(test)]
+mod tests {
+
+    use chrono::NaiveDate;
+    use regex::Regex;
+
+    use crate::gitlab::{
+        AccessLevel, AccessToken, AccessTokenScope, PersonalAccessToken, PersonalAccessTokenScope,
+        Token,
+    };
+
+    //
+    // Utility functions
+    //
+    fn get_first_non_comment_line(text: &str) -> &str {
+        text.lines().find(|line| !line.starts_with('#')).unwrap()
+    }
+
+    fn default_project_token() -> Token {
+        Token::Project {
+            token: AccessToken {
+                access_level: AccessLevel::Guest,
+                active: true,
+                expires_at: NaiveDate::parse_from_str("2199-06-29", "%Y-%m-%d").unwrap(),
+                name: "name".to_string(),
+                revoked: false,
+                scopes: vec![AccessTokenScope::Api],
+            },
+            full_path: "whatever".to_string(),
+            web_url: "whatever".to_string(),
+        }
+    }
+
+    fn default_group_token() -> Token {
+        Token::Group {
+            token: AccessToken {
+                access_level: AccessLevel::Guest,
+                active: true,
+                expires_at: NaiveDate::parse_from_str("2199-06-29", "%Y-%m-%d").unwrap(),
+                name: "name".to_string(),
+                revoked: false,
+                scopes: vec![AccessTokenScope::Api],
+            },
+            full_path: "whatever".to_string(),
+            web_url: "whatever".to_string(),
+        }
+    }
+
+    fn default_user_token() -> Token {
+        Token::User {
+            token: PersonalAccessToken {
+                active: true,
+                expires_at: NaiveDate::parse_from_str("2199-06-29", "%Y-%m-%d").unwrap(),
+                name: "name".to_string(),
+                revoked: false,
+                scopes: vec![PersonalAccessTokenScope::Api],
+                user_id: 123,
+            },
+            full_path: "whatever".to_string(),
+        }
+    }
+
+    #[test]
+    /// Ensures that the group token metric name begins with "gitlab_token_"
+    fn group_token_metric_name_start() {
+        let token = default_group_token();
+        let text = crate::prometheus_metrics::build(token).unwrap();
+        let metric = get_first_non_comment_line(&text);
+        assert!(metric.starts_with("gitlab_token_"));
+    }
+
+    #[test]
+    /// Ensures that the project token metric name begins with "gitlab_token_"
+    fn project_token_metric_name_start() {
+        let token = default_project_token();
+        let text = crate::prometheus_metrics::build(token).unwrap();
+        let metric = get_first_non_comment_line(&text);
+        assert!(metric.starts_with("gitlab_token_"));
+    }
+
+    #[test]
+    /// Ensures that the user token metric name begins with "gitlab_token_"
+    fn user_token_metric_name_start() {
+        let token = default_user_token();
+        let text = crate::prometheus_metrics::build(token).unwrap();
+        let metric = get_first_non_comment_line(&text);
+        assert!(metric.starts_with("gitlab_token_"));
+    }
+}

e3b2304bd3ee80051885fb9946f882341dad9d2d
sbrr <romain.siebenhuner@cnieg.fr>
2026-07-21 15:19:13 +0200

    perf: move regex definition in CONFIG (to instantiate once)

--- src/state_actor.rs
+++ src/state_actor.rs
@@ -1,7 +1,6 @@
 //! This is the main actor, it handles all [`Message`]
 
 use anyhow::Context as _;
-use regex::Regex;
 use std::collections::HashMap;
 use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinSet;
@@ -170,11 +169,9 @@ async fn get_users_tokens_metrics() -> Result<String, anyhow::Error> {
             time.elapsed()
         );
 
-        let bot_users_re = Regex::new("(project|group)_[0-9]+_bot_[0-9a-f]{32,}")
-            .context("failed to compile bot_users_re regex")?;
         let user_ids: HashMap<_, _> = users
             .iter()
-            .filter(|user| !bot_users_re.is_match(&user.username))
+            .filter(|user| !CONFIG.bot_users_re.is_match(&user.username))
             .filter(|user| match &CONFIG.usernames_filter {
                 Some(filter) => filter.contains(&user.username),
                 None => true,
